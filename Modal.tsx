8a3919be15740d11d32d4c6403926234d7c81c38
Yue <yue@potentia.asia>
2022-11-15 16:07:35 +0800

    add Select component

--- Modal.tsx
+++ Modal.tsx
@@ -1,12 +1,15 @@
 import { ModalUnstyled, ModalUnstyledProps } from '@mui/base';
+import classNames from 'classnames';
 import Backdrop from './Backdrop';
 import IcClose from './image/ic-close.svg';
 
 type Props = ModalUnstyledProps & {
   handleClose: () => void;
+  showClose?: boolean;
+  px?: boolean;
 };
 
-const Modal = ({ children, handleClose, ...props }: Props) => (
+const Modal = ({ children, handleClose, showClose = true, px = true, ...props }: Props) => (
   <ModalUnstyled
     className="z-10 fixed right-0 bottom-0 top-0 left-0 flex items-center justify-center"
     slots={{ backdrop: Backdrop }}
@@ -14,14 +17,23 @@ const Modal = ({ children, handleClose, ...props }: Props) => (
     {...props}
   >
     <div className="w-full mx-[15px] sm:w-[610px] sm:mx-auto bg-white rounded-[20px] outline-none">
-      <div className="relative h-[34px]">
-        <img
-          className="absolute right-[10px] bottom-0 cursor-pointer"
-          src={IcClose}
-          onClick={handleClose}
-        />
+      {showClose && (
+        <div className="relative h-[34px]">
+          <img
+            className="absolute right-[10px] bottom-0 cursor-pointer"
+            src={IcClose}
+            onClick={handleClose}
+          />
+        </div>
+      )}
+      <div
+        className={classNames('pb-[20px]', {
+          'pt-[20px]': !showClose,
+          'px-5': px,
+        })}
+      >
+        {children}
       </div>
-      <div className="px-5 pb-[20px]">{children}</div>
     </div>
   </ModalUnstyled>
 );
---
+++ Select.tsx
@@ -0,0 +1,80 @@
+import classNames from 'classnames';
+import { isValidElement, ReactNode, useMemo, useState } from 'react';
+import SelectContext from './context/SelectContext';
+import IcSelectDisabled from './image/ic-select-disabled.svg';
+import IcSelect from './image/ic-select.svg';
+import Modal from './Modal';
+
+/**
+ * usage example:
+ *
+ * <Select>
+ *   <SelectOption value="aa">a</SelectOption>
+ *   <SelectOption value="bb">b</SelectOption>
+ *   <SelectOption value="cc">c</SelectOption>
+ * </Select>
+ */
+
+type Props = {
+  children: ReactNode | ReactNode[];
+  label?: string;
+  disabled?: boolean;
+  value?: string;
+  onChange?: (v: string) => void;
+};
+
+const Select = ({ children, label, disabled, onChange, value: controlledSelectedValue }: Props) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [selected, setSelected] = useState<string>('');
+
+  const options = useMemo(
+    () =>
+      (Array.isArray(children) ? children : [children]).map((child) =>
+        isValidElement(child) ? child.props : null,
+      ),
+    [],
+  );
+
+  const handleChange = (v: string) => {
+    setOpen(false);
+    if (controlledSelectedValue === undefined) setSelected(v);
+
+    onChange && onChange(v);
+  };
+
+  return (
+    <SelectContext.Provider value={{ current: controlledSelectedValue ?? selected, handleChange }}>
+      <div className="w-full">
+        {label && (
+          <div
+            className={classNames('text-[14px] leading-normal text-navy-700 mb-[5px]', {
+              'opacity-30': disabled,
+            })}
+          >
+            {label}
+          </div>
+        )}
+        <div
+          className={classNames(
+            'rounded bg-grey-200 outline-none p-2 h-[40px] cursor-pointer flex justify-between',
+            {
+              'text-grey-500 placeholder:text-grey-500': !!disabled,
+              'text-navy-900 placeholder:text-navy-100': !disabled,
+            },
+          )}
+          onClick={() => setOpen(true)}
+        >
+          <div>
+            {options.find((v) => v.value === (controlledSelectedValue ?? selected))?.children}
+          </div>
+          <div>{disabled ? <img src={IcSelect} /> : <img src={IcSelectDisabled} />}</div>
+        </div>
+        <Modal open={open} handleClose={() => setOpen(false)} showClose={false} px={false}>
+          <>{children}</>
+        </Modal>
+      </div>
+    </SelectContext.Provider>
+  );
+};
+
+export default Select;
