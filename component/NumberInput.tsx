be5232822a7fc4d025ee8dab775ad0cc1a8d6edc
Yue <yue@potentia.asia>
2023-01-05 14:41:41 +0800

    consider negative amount

--- component/NumberInput.tsx
+++ component/NumberInput.tsx
@@ -3,16 +3,23 @@ import Input, { Props as InputProps } from './Input';
 
 export type Props = InputProps & {
   decimal: number;
+  allowNegative?: boolean;
 };
 
-const NumberInput = forwardRef<HTMLInputElement, Props>(({ decimal, ...props }, ref) => (
-  <Input
-    regex={decimal > 0 ? new RegExp(`^[0-9]*\\.?[0-9]{0,${decimal}}$`) : new RegExp('^[0-9]*$')}
-    ref={ref}
-    inputMode="numeric"
-    {...props}
-  />
-));
+const NumberInput = forwardRef<HTMLInputElement, Props>(
+  ({ decimal, allowNegative = false, ...props }, ref) => (
+    <Input
+      regex={
+        decimal > 0
+          ? new RegExp(`^${allowNegative ? '-?' : ''}[0-9]*\\.?[0-9]{0,${decimal}}$`)
+          : new RegExp('^[0-9]*$')
+      }
+      ref={ref}
+      inputMode="numeric"
+      {...props}
+    />
+  ),
+);
 
 NumberInput.displayName = 'NumberInput';
 
